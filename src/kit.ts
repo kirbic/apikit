d39c808081c3fcb7ce9c75238d008ff5ab7b4633
Marc Pomar <marc@corecode.school>
2021-04-27 17:38:21 +0200

    fix: kirbic api wrap

--- src/kit.ts
+++ src/kit.ts
@@ -1,5 +1,7 @@
+import { Awaited } from "ts-essentials";
 import { get_api, ApiConfig } from "./api";
 import { Endpoints } from "@kirbic/types";
+import { AxiosError, AxiosInstance } from "axios";
 
 export type Cart = Endpoints["GET /cart/"]["response"]["data"];
 
@@ -8,64 +10,62 @@ export type Currency = Price["currency"];
 export type Unit = Price["unit_type"];
 export type CartActionMode = Endpoints["PATCH /cart/{mode}/{price_id}"]["parameters"]["mode"];
 export type ProductCreateParams = Endpoints["POST /catalog/product/"]["parameters"];
+export type Product = Endpoints["POST /catalog/product/"]["response"]["data"];
 
-export type ApiKit = ReturnType<typeof kirbic_api_kit>;
+const api_error = async <T>(arg0: T): Promise<T> => {
+  try {
+    return await arg0;
+  } catch (error) {
+    if (error.isAxiosError) {
+      const e: AxiosError = error;
+      console.error(`❌ Kirbic API Error [${e.response?.status}]`);
+      console.error(e.response?.data);
+    } else {
+      console.log("❌ Kirbic API Error");
+    }
+    throw error;
+  }
+};
 
-// type PROMFN = (...args: any) => Promise<any>;
-// const api_error = <T extends PROMFN>(fn: T) => async (
-//   ...args: Parameters<T>
-// ) => {
-//   try {
-//     console.log("calling api");
-//     return await fn(...args);
-//   } catch (error) {
-//     console.log("❌ API ERROR");
-//     if (error.isAxiosError) {
-//       console.log(error.response.data);
-//     }
-//   }
-// };
+export class KirbicApiKit {
+  api: AxiosInstance;
+  constructor(config: ApiConfig) {
+    this.api = get_api(config);
+  }
 
-export const kirbic_api_kit = (config: ApiConfig) => {
-  const api = get_api(config);
-
-  const get_cart = async (): Promise<Cart> => {
-    const res = await api.get<Cart>("/cart");
+  async get_cart() {
+    const res = await api_error(this.api.get<Cart>("/cart"));
     return res.data;
-  };
+  }
 
-  const delete_cart = async (): Promise<void> => {
-    await api.delete("/cart/");
-  };
-
-  const set_metadata = async (
-    metadata: Record<string, unknown>
-  ): Promise<Cart> => {
-    const res = await api.post("/cart/metadata/", metadata);
+  async set_metadata(metadata: Record<string, unknown>) {
+    const res = await api_error(
+      this.api.post<Cart>("/cart/metadata/", metadata)
+    );
     return res.data;
-  };
+  }
 
-  const cart_action_api = async (
+  async cart_action_api(
     mode: CartActionMode,
     price_id: string,
     quantity: number
-  ): Promise<Cart> => {
-    const res = await api.patch<Cart>(`/cart/${mode}/${price_id}`, {
-      quantity,
-    });
+  ) {
+    const res = await api_error(
+      this.api.patch<Cart>(`/cart/${mode}/${price_id}`, {
+        quantity,
+      })
+    );
     return res.data;
-  };
+  }
 
-  const create_product = async (data: ProductCreateParams) => {
-    const product = await api.post("/catalog/product", data);
+  async delete_cart() {
+    await api_error(this.api.delete("/cart/"));
+  }
+
+  async create_product(data: ProductCreateParams) {
+    const product = await api_error(
+      this.api.post<Product>("/catalog/product", data)
+    );
     return product.data;
-  };
-
-  return {
-    cart_action_api,
-    delete_cart,
-    get_cart,
-    set_metadata,
-    create_product,
-  };
-};
+  }
+}
